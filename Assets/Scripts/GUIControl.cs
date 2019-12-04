92b9c793a297e5282bcc323c8c02cd03cc7ed9e8
ChristopherHodapp <hodapp.christopher@gmail.com>
2019-12-04 12:24:05 -0700

    initial commit

---
+++ Assets/Scripts/GUIControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIControl : MonoBehaviour
+{
+    public Canvas playCanvas;
+    public Canvas settingsCanvas;
+    public Canvas creditsCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playCanvas.enabled = false;
+        settingsCanvas.enabled = false;
+        creditsCanvas.enabled = false;
+    }
+
+
+    public void showPlayCanvas()
+    {
+        playCanvas.enabled = true;
+    }
+    public void hidePlayCanvas()
+    {
+        playCanvas.enabled = false;
+    }
+    public void showSettingsCanvas()
+    {
+        settingsCanvas.enabled = true;
+    }
+    public void hideSettingsCanvas()
+    {
+        settingsCanvas.enabled = false;
+    }
+    public void showCreditsCanvas()
+    {
+        creditsCanvas.enabled = true;
+    }
+    public void hideCreditsCanvas()
+    {
+        creditsCanvas.enabled = false;
+    }
+}
