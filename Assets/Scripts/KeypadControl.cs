6ac609836cb9b1b0110d031abb7b615868f2dc7b
stryder03 <stryder03@users.noreply.github.com>
2019-12-06 09:13:53 -0700

    merged room2

---
+++ Assets/Scripts/KeypadControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeypadControl : MonoBehaviour
+{
+    public Canvas KeypadCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        KeypadCanvas.enabled = false;
+
+    }
+
+        public void showKeypadCanvas()
+        {
+            KeypadCanvas.enabled = true;
+        }
+        public void hideKeypadCanvas()
+        {
+            KeypadCanvas.enabled = false;
+        }
+    
+}
